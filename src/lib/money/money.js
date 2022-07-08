16d06851ddd368d3905a0d79e3015169319e2961
Hossein Alipour <hossein.4lipour@gmail.com>
2022-07-09 00:45:27 +0300

    add money class to handle finantial operators

---
+++ src/lib/money/money.js
@@ -0,0 +1,79 @@
+/**
+ * money class to handle subset math operators.
+ * this is due to inaccurecy in IEEE float number
+ * which is implemented in most programming languages
+ */
+
+class Money {
+  constructor(amount, currency) {
+    const parsedAmount = Number.parseInt(amount, 10);
+    if (Number.isNaN(parsedAmount) || amount < 0) {
+      throw new Error('money must be valid number larger or equal to zero');
+    }
+    this.parsePriceToInt(amount);
+    this.currency = currency;
+  }
+
+  /**
+   * parse optional float to int and a precision
+   * @param {number|string} amount
+   */
+  parsePriceToInt(amount) {
+    const amountIntLength = `${Number.parseInt(amount, 10)}`.length;
+    const amountLength = `${amount}`.length;
+
+    if (amountLength > amountIntLength) {
+      // float provided convert float to str inteager
+      const [int, float] = `${amount}`.split('.');
+      this.amount = +(int + float);
+      this.precision = amountLength - 1 - amountIntLength;
+    } else {
+      this.amount = amount;
+      this.precision = 0;
+    }
+  }
+
+  static validate(input) {
+    if (input instanceof Money) return input;
+    return new Money(input);
+  }
+
+  multiply(multiplyAmount) {
+    const moneyInstance = Money.validate(multiplyAmount);
+
+    this.amount *= moneyInstance.amount;
+    this.precision += moneyInstance.precision;
+  }
+
+  add(addAmount) {
+    const moneyInstance = Money.validate(addAmount);
+
+    this.amount += moneyInstance.amount;
+    this.precision = Math.max(this.precision, moneyInstance.precision);
+  }
+
+  get value() {
+    const decimalFactor = `e-${this.precision}`;
+    const price = this.amount + decimalFactor;
+    return price * 1;
+  }
+
+  /**
+   * format integer to float type to the nth decimal place
+   * @param {decimalPlace = 2} decimalPlace
+   */
+  format(decimalPlace = 2) {
+    // use number exponent to handle money
+    const ceilFactor = `e${decimalPlace - this.precision}`;
+    const decimalFactor = `e-${decimalPlace}`;
+
+    const price = Math.ceil(this.amount + ceilFactor) + decimalFactor;
+
+    // multiply price by one to remove exponent from number
+    return price * 1;
+  }
+}
+
+export default function money(...args) {
+  return new Money(...args);
+}
---
+++ src/lib/money/money.test.js
@@ -0,0 +1,122 @@
+/* eslint-disable no-undef */
+import { expect } from 'chai';
+import $ from './money';
+
+describe('should handle different inputs types', () => {
+  it('should handle short floating inputs', () => {
+    const money = $(0.2);
+    expect(money.format()).to.equal(0.2);
+    expect(money.value).to.equal(0.2);
+  });
+
+  it('should handle long floating inputs', () => {
+    const money = $(0.2585);
+    expect(money.format()).to.equal(0.26);
+    expect(money.value).to.equal(0.2585);
+  });
+
+  it('should handle short integer inputs', () => {
+    const money = $(23);
+    expect(money.format()).to.equal(23);
+  });
+
+  it('should handle large integer inputs', () => {
+    const money = $(23);
+    expect(money.format()).to.equal(23);
+  });
+});
+
+describe('should format numbers correctly', () => {
+  it('should format 0.2023 ceiled to the 2ed decimal place to yeild 0.21', () => {
+    const money = $(0.2023);
+
+    expect(money.format(2)).to.equal(0.21);
+    expect(money.value).to.equal(0.2023);
+  });
+
+  it('should format 0.2023 ceiled to the 3rd decimal place to yeild 0.203', () => {
+    const money = $(0.2023);
+
+    expect(money.format(3)).to.equal(0.203);
+  });
+});
+
+describe('should add numbers correctly', () => {
+  it('should add up 0.1 and 0.2 to 0.3', () => {
+    const money = $(0.1);
+
+    money.add(0.2);
+
+    expect(money.format()).to.equal(0.3);
+  });
+
+  it('should add up 0.875 and 0.125 to 1', () => {
+    const money = $(0.875);
+
+    money.add(0.125);
+
+    expect(money.format()).to.equal(1);
+  });
+});
+
+describe('should multiply numbers correctly', () => {
+  it('should multiply 0.1 * 0.2 to yeild 0.01', () => {
+    const money = $(0.1);
+
+    money.multiply(0.2);
+
+    expect(money.format()).to.equal(0.02);
+  });
+
+  it('should multiply 13 * 0.7 to yeild 9.1', () => {
+    const money = $(13);
+
+    money.multiply(0.7);
+
+    expect(money.format()).to.equal(9.1);
+  });
+
+  it('should multiply 3 to 0.15 to yeild 0.45', () => {
+    const money = $(3);
+
+    money.multiply(0.15);
+
+    expect(money.format()).to.equal(0.45);
+  });
+
+  it('should multiply 0.15 to 3 to yeild 0.45', () => {
+    const money = $(0.15);
+
+    money.multiply(3);
+
+    expect(money.format()).to.equal(0.45);
+  });
+});
+
+describe('should handle input type for itself', () => {
+  it('add', () => {
+    const money = $(0.875);
+
+    money.add($(0.125));
+
+    expect(money.format()).to.equal(1);
+  });
+
+  it('multiply', () => {
+    const money = $(13);
+
+    money.multiply($(0.7));
+
+    expect(money.format()).to.equal(9.1);
+  });
+});
+
+describe('should throw', () => {
+  it('expect non-number argument to throw error', () => {
+    expect(() => $('foo')).to.throw();
+  });
+
+  it('expect empty argument to throw error ', () => {
+    expect(() => $()).to.throw();
+  });
+});
