89582fbced99cd1f4750e07e1b87c557eb8d2a51
Hossein Alipour <hossein.4lipour@gmail.com>
2022-07-09 15:12:44 +0300

    add fake db instance

---
+++ src/lib/db/fakeDbInterface.js
@@ -0,0 +1,32 @@
+/** a singleton (module) to store transaction */
+import { isSameWeek } from 'date-fns';
+import $ from '../money/money';
+
+class FakeDbInterface {
+  constructor() {
+    this.collection = [];
+  }
+
+  insert(obj) {
+    this.collection.push(obj);
+  }
+
+  getThisWeekTotalTransaction(type, { userId, date, amount }) {
+    const total = $(0);
+    this.collection.forEach((entry) => {
+      const sameUserId = entry.userId === userId;
+      const sameType = entry.type === type;
+      const happenedThisWeek = isSameWeek(new Date(date), new Date(entry.date), {
+        weekStartsOn: 1 /* 0 is Sunday, 1 is Monday */,
+      });
+
+      if (sameUserId && happenedThisWeek && sameType) {
+        return total.add(entry.amount);
+      }
+    });
+
+    return total.value;
+  }
+}
+
+export default new FakeDbInterface();
