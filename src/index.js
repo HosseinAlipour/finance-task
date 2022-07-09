38cb22d5c02477535fd3c158dbb033efe22201d4
Hossein Alipour <hossein.4lipour@gmail.com>
2022-07-09 15:13:28 +0300

    load and remap entry file data

--- src/index.js
+++ src/index.js
@@ -7,5 +7,26 @@ const [inputFilePath] = process.argv.slice(2);
 (async () => {
   const inputJson = (await import(path.resolve(inputFilePath))).default;
 
-  console.log(inputJson);
+  inputJson.forEach((request) => {
+    // remap request to CamelCase, due to airbnb eslint rules
+    const {
+      user_type: userType,
+      user_id: userId,
+      type,
+      operation: { amount, currency },
+      date,
+    } = request;
+
+    const remapRequest = {
+      userType,
+      userId,
+      type,
+      amount,
+      currency,
+      date,
+    };
+
+    // commit to db
+    fakeDbInterface.insert({ ...remapRequest, commission });
+  });
 })();
